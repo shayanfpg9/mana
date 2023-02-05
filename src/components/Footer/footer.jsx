bff138a3e0df8b726e6109d8f542f13f962639e3
shayanfpg9 <shfqaen@gmail.com>
2023-02-05 16:44:17 +0330

    make footer

---
+++ src/components/Footer/footer.jsx
@@ -0,0 +1,26 @@
+import { $ } from "../../js/functions";
+
+export default function Footer() {
+  const heartClick = ({ target }) => {
+    target.classList.toggle("bi-heart");
+    target.classList.toggle("bi-heart-fill");
+    if (target.classList.toggle("clicked"))
+      window?.localStorage.setItem("heart-status", "clicked");
+    else window?.localStorage.setItem("heart-status", "default");
+  };
+
+  window.onload = () => {
+    if (window?.localStorage.getItem("heart-status") === "clicked")
+      heartClick({ target: $(".footer__icon--heart") });
+  };
+
+  return (
+    <footer className="footer">
+      <i className="bi bi-c-circle"></i> copyright&nbsp;
+      {new Date().getFullYear()} -
+      <a className="footer__link-samcode" href="https://github.com/sampadycode"> SampadyCode </a>
+      <i onClick={heartClick} className="bi bi-heart footer__icon--heart"></i>
+      <i className="bi bi-cup-hot-fill footer__icon--cup"></i>
+    </footer>
+  );
+}
