0503358157c1bf774167ae3705afe6e4fd85f0bd
shayanfpg9 <shfqaen@gmail.com>
2023-02-08 22:09:59 +0330

    make searchbox

---
+++ src/components/Header/Search.jsx
@@ -0,0 +1,45 @@
+import { useEffect } from "react";
+import { $ } from "../../js/functions";
+
+export default function Search(props) {
+  useEffect(() => {
+    if (props.open) {
+      $(".searchbox").addEventListener("keyup", ({ code }) => {
+        console.log(code);
+        if (code == "Escape") props.close();
+      });
+      $(".searchbox__input").focus();
+    }
+  });
+
+  return (
+    <>
+      <div onClick={props.close} className={props.open ? "blur" : "hide"}>
+        <button
+          onClick={props.close}
+          type="button"
+          className="bi bi-x searchbox__close"
+        ></button>
+      </div>
+
+      <form
+        action={props.path}
+        method="get"
+        role="searchbox"
+        className={"searchbox " + (!props.open ? "hide" : "")}
+      >
+        <input
+          role="search"
+          className="searchbox__input"
+          type="text"
+          name="team"
+          placeholder="نام تیم را وارد کنید"
+        />
+        <button className="searchbox__submit" type="submit">
+          <i className="bi bi-search-heart-fill"></i>
+          پیدا کن
+        </button>
+      </form>
+    </>
+  );
+}
