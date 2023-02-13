990ca398ab0d6912d13308fa0bb2b9adf86fcf86
shayanfpg9 <shfqaen@gmail.com>
2023-02-13 22:00:43 +0330

    noreferrer

--- src/components/Footer/Contact.jsx
+++ src/components/Footer/Contact.jsx
@@ -1,6 +1,6 @@
 export default function Contact({ link }) {
   return (
-    <a className="contact" href={link} target="_blank">
+    <a className="contact" href={link} target="_blank" rel="noreferrer">
       <i className="bi bi-chat-dots-fill contact__icon"></i>
     </a>
   );
