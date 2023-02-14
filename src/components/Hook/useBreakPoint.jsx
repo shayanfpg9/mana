44d6b68ca718a421d9f1bf89d0b39fc963a996eb
shayanfpg9 <shfqaen@gmail.com>
2023-02-14 22:25:57 +0330

    fix hook

--- src/components/Hook/useBreakPoint.jsx
+++ src/components/Hook/useBreakPoint.jsx
@@ -1,12 +1,19 @@
 import { useState } from "react";
-import useSize from "./useSize";
+import { useSize } from "./hooks";
 
 export default function useBreakPoint(init = "now") {
   const [breakpoint, setBreackpoint] = useState(""),
-    [size, setSize] = useState(init);
+    [size, setSize] = useState(init),
+    sizes = {
+      xxl: useSize("xxl"),
+      xl: useSize("xl"),
+      md: useSize("md"),
+      sm: useSize("sm"),
+      xs: useSize("xs"),
+    };
 
   if (typeof init == "string") {
-    if (init == "now") {
+    if (init === "now") {
       setSize(window?.innerWidth);
       window?.addEventListener("resize", () => {
         setSize(window?.innerWidth);
@@ -16,13 +23,10 @@ export default function useBreakPoint(init = "now") {
     }
   }
 
-  if (size >= useSize("xxl")) return "xxl";
-  else if (size >= useSize("xl") && size < useSize("xxl"))
-    setBreackpoint("xl");
-  else if (size >= useSize("md") && size < useSize("xl"))
-    setBreackpoint("md");
-  else if (size >= useSize("sm") && size < useSize("md"))
-    setBreackpoint("sm");
+  if (size >= sizes.xxl) return "xxl";
+  else if (size >= sizes.xl && size < sizes.xxl) setBreackpoint("xl");
+  else if (size >= sizes.md && size < sizes.xl) setBreackpoint("md");
+  else if (size >= sizes.sm && size < sizes.md) setBreackpoint("sm");
   else setBreackpoint("xs");
 
   return breakpoint;
