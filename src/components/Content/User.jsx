0429dd132d5c40b9c6d1a5da99e41402a10f71c2
shayanfpg9 <shfqaen@gmail.com>
2023-02-15 21:51:30 +0330

    make user component

---
+++ src/components/Content/User.jsx
@@ -0,0 +1,20 @@
+import useTraUser from "../Hook/useTraUser";
+
+export default function User(props) {
+  const info = useTraUser(props);
+
+  return (
+    <div
+      className={`user ${info.level === "else" ? `user--${info.level}` : ""}`}
+    >
+      <span className="user__team">{info.team}</span>
+      <img className="user__image" src={info.image} alt={info.team} />
+      <div style={{ width: info.aim }} className="user__payment">
+        <span
+          style={{ width: `${(info.aim / info.payment) * 100}%` }}
+          className="user__payment--prog"
+        ></span>
+      </div>
+    </div>
+  );
+}
