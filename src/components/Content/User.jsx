061e16a04ed2f171edd09071b2f9f54a1c7a8590
shayanfpg9 <shfqaen@gmail.com>
2023-02-15 23:05:41 +0330

    fix aim + image

--- src/components/Content/User.jsx
+++ src/components/Content/User.jsx
@@ -8,10 +8,12 @@ export default function User(props) {
       className={`user ${info.level !== "else" ? `user--${info.level}` : ""}`}
     >
       <span className="user__team">{info.team}</span>
-      <img className="user__image" src={info.image} alt={info.team} />
-      <div style={{ width: info.aim }} className="user__payment">
+      <div className="user__image">
+        <img src={info.image} alt={info.team} />
+      </div>
+      <div className="user__payment">
         <span
-          style={{ width: `${(info.aim / info.payment) * 100}%` }}
+          style={{ width: `${(100 * info.payment) / info.aim}%` }}
           className="user__payment--prog"
         ></span>
       </div>
