34e40629c107989ea4006978f53e9b1ef6bc6d84
shayanfpg9 <shfqaen@gmail.com>
2023-02-17 14:02:05 +0330

    connect to JSON file

--- src/components/Content/Parent.jsx
+++ src/components/Content/Parent.jsx
@@ -1,25 +1,35 @@
 import { useState } from "react";
 import User from "./User";
-import json from "../../db/users.json";
+import axios from "axios";
 
-export default function Parent() {
-  json.sort((a, b) => {
-    return b.payment - a.payment;
-  });
-
-  const [data, setData] = useState(json),
+export default function Parent({ api }) {
+  const [data, setData] = useState(null),
     els = data?.map((val, i) => {
       return <User key={`user-${i + 1}`} {...val} index={i} />;
     });
 
-  window?.addEventListener("load", () => {
-    scroll(document.querySelectorAll(".user"));
-  });
+  axios
+    .get(api, {
+      timeout: 3000,
+    })
+    .then((res) => {
+      if (data === null) {
+        setData(res.data.sort(sort).slice(0, 20));
+        scroll(document.querySelectorAll(".user"));
+      } else if (!equal(res.data, data)) {
+        setData(res.data.sort(sort).slice(0, 20));
+        res.data.forEach((obj, i) => {
+          if (!equal(obj, data[i])) {
+            document.querySelectorAll(".user")[i]?.scrollIntoView();
+          }
+        });
+      }
+    });
 
   return <main className="parent">{els}</main>;
 }
 
-function scroll(elements) {
+function scroll(elements, stop = 10000) {
   window?.scroll(0, 0);
 
   let ComponentH = 0;
@@ -30,7 +40,7 @@ function scroll(elements) {
     });
 
   const transform =
-    ~~((window?.innerHeight - ComponentH) / (elements[1].clientHeight + 10)) -
+    ~~((window?.innerHeight - ComponentH) / (elements[1]?.clientHeight + 10)) -
     1; // index 1 because in the index 0 for the large width maybe we're don't have overflow
 
   let i = transform;
@@ -38,12 +48,23 @@ function scroll(elements) {
   window?.setTimeout(() => {
     const inter = window?.setInterval(() => {
       if (i + transform <= elements.length) {
-        elements[i].scrollIntoView();
+        elements[i]?.scrollIntoView();
         i += transform;
       } else {
         window?.clearInterval(inter);
         scroll(elements);
       }
     }, 5000);
-  }, 10000);
+  }, stop);
+}
+
+function sort(a, b) {
+  return b.payment - a.payment;
+}
+
+function equal(a, b) {
+  a = JSON.stringify(a);
+  b = JSON.stringify(b);
+
+  return a === b;
 }
