0b493f262ac65045d602f92b26c60f3015d27ffa
shayanfpg9 <shfqaen@gmail.com>
2023-02-18 22:19:52 +0330

    route the page

--- src/components/Mana.jsx
+++ src/components/Mana.jsx
@@ -2,16 +2,23 @@ import { Component } from "react";
 import Header from "./Header/Header";
 import Footer from "./Footer/footer";
 import Parent from "./Content/Parent";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 export default class Mana extends Component {
   render() {
     document.body.classList.add("light");
     return (
-      <>
+      <Router>
         <Header />
-        <Parent api="./db/users.json" />
+        <Routes>
+          <Route path="/" element={<Parent api="./db/users.json" />} />
+          <Route
+            path="/team/:team"
+            element={<Parent api="../../db/users.json" />}
+          />
+        </Routes>
         <Footer />
-      </>
+      </Router>
     );
   }
 }
